377ad569700961935288309cb032745d74809244
eradication0 <jakob.minka@gmail.com>
2016-12-20 17:21:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,30 @@
 console.log('<== STARTING MODULAR BOT ==>');
 
 // Loading Requirements
-const discord = require('discord.js') // Discord itself
-const settings = require('./settings.json') // Settings file
-const fs = require('fs') // Filesystem npm Package
-const bot = new discord.Client() // Creating a new bot instance
+const discord = require('discord.js')
+const settings = require('./settings.json')
+const fs = require('fs')
+const bot = new discord.Client()
 
 console.log('Setup √')
 
-// Looping through all commands
 bot.on('message', (message) => {
-	if (message.author.id === bot.user.id) return // Bot should not listen to himself
-	if (!message.content.startsWith(cred.prefix)) return // Bot only executes command if the right prefix is used
-	const args = message.content.split(' '); // Splits message parts into arguments
-	const command = args.shift().slice(cred.prefix.length); // Deletes the prefix in the command variable
+	if (message.author.id === bot.user.id) return
+	if (!message.content.startsWith(settings.prefix)) return
+	const args = message.content.split(' ')
+	const command = args.shift().slice(settings.prefix.length)
 		try {
-			let cmdFile = require('./commands/' + command); // requires all files in the commands folder
-			cmdFile.run(bot, message, args); // Runs the command
+			let cmdFile = require('./commands/' + command)
+			cmdFile.run(bot, message, args)
 		} catch (e) {
-			fileLog(e + '\n'); // Occuring errors will be stored in the log.txt
+			console.log(e + '\n')
 	}
 })
 
 console.log('Commands loaded √')
 
-// "When bot is online" function
 bot.on('ready', () => {
     console.log('<== BOT ONLINE ==>')
 })
 
-// Logs in via Token (Set in the settings.json file)
 bot.login(settings.bottoken)
