7a9c678121d73e007bd29ff7571e9c6a1e168be1
Zer0 <jakob.minka@gmail.com>
2017-01-06 15:50:34 +0100

    finalization

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ console.log('<== STARTING MODULAR BOT ==>');
 
 // Loading Requirements
 const discord = require('discord.js') // Discord itself
-const settings = require('settings.js') // Settings file
+const settings = require('./settings.json') // Settings file
 const fs = require('fs') // Filesystem npm Package
 const bot = new discord.Client() // Creating a new bot instance
 
@@ -11,24 +11,32 @@ console.log('Setup √')
 // Looping through all commands
 bot.on('message', (message) => {
 	if (message.author.id === bot.user.id) return // Bot should not listen to himself
-	if (!message.content.startsWith(cred.prefix)) return // Bot only executes command if the right prefix is used
+	if (message.content.startsWith('.eval') && message.author.id === 'YOUR ID HERE!') {
+		try {
+			const com = eval(message.content.split(" ").slice(1).join(" "))
+			message.channel.sendMessage('```\n' + com + '```')
+		} catch (e) {
+			message.channel.sendMessage('```\n' + e + '```')
+		}
+	}
+	if (!message.content.startsWith(settings.prefix)) return // Bot only executes command if the right prefix is used
 	const args = message.content.split(' '); // Splits message parts into arguments
-	const command = args.shift().slice(cred.prefix.length); // Deletes the prefix in the command variable
+	const command = args.shift().slice(settings.prefix.length); // Deletes the prefix in the command variable
 	try {
 		let cmdFile = require('./commands/' + command); // requires all files in the commands folder
 		cmdFile.run(bot, message, args); // Runs the command
 	} catch (e) {
-		fileLog(e + '\n'); // Occuring errors will be stored in the log.txt
+		console.log(e + '\n');
 	}
-}
+
+})
 
 console.log('Commands loaded √')
 
 // "When bot is online" function
 bot.on('ready', () => {
-    console.log('<== STARTING MODULAR BOT ==>')
+    console.log('<== MODULAR BOT ONLINE ==>')
 })
 
 // Logs in via Token (Set in the settings.json file)
 bot.login(settings.bottoken)
-console.log('<== BOT ONLINE ==>')
